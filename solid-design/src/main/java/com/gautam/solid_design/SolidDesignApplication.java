3289e7486d8ae5b35363c3cfb1f0877fecbf9a9c
Gautam kumar <Gautam.kumar@nike.com>
2024-09-21 20:30:37 +0530

    OOD

---
+++ solid-design/src/main/java/com/gautam/solid_design/SolidDesignApplication.java
@@ -0,0 +1,13 @@
+package com.gautam.solid_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidDesignApplication.class, args);
+	}
+
+}
---
+++ solid-design/src/test/java/com/gautam/solid_design/SolidDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gautam.solid_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
